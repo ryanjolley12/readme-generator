121f967fd779ccb3b942e2322c76297a26f18dd6
ryanjolley12 <ryanswims@gmail.com>
2021-07-18 11:47:38 -0400

    add generateMarkdown and initialize

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require ("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required) ",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+         } else  {
+             console.log("You must enter the name of your project.");
+             return false;
+         }
+            
+        }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please describe your project (Required)",
+            validate: descriptionInput => {
+                (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("You must enter a project description.");
+                    return false;
+                }
+            }
+            },
+    {
+        type: 'rawlist',
+        name: 'lisence',
+        message: 'Which license does your project need?',
+        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC License', 'None']
+        validate: license => {
+            if (license) {
+                return true;
+            } else if (!license){
+                console.log('Please enter required information');
+                return false;
+            }
+        }
+    },
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(questions => {
+
+        const markupHtml = writeToFile(questions);
+        fs.writeFile("./dist/README.md", markupHtml, err =>{
+            if(err){
+                console.log(err);
+            } else {
+                console.log('Success!');
+            }
+        })
+    })
+    
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,41 @@
+const fs = require("fs");
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseLink) {
+  if (licenseLink) {
+    return `${licenseLink}`
+  } else {
+      return '';
+    }
+  }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `${license}`
+   } else {
+      return "";
+    }
+  }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # Project Title 
+  ## ${data.title}
+
+  ## ${data.description}
+
+  ### ${data}
+
+
+
+
 
 `;
 }
