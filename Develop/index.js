86e9820648d37ccf892f3ebb68339c0be0fe1759
ryanjolley12 <ryanswims@gmail.com>
2021-07-19 19:21:23 -0400

    geenerate markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require ("fs");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown");
+const { type } = require("os");
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -25,32 +26,52 @@ const questions = () => {
             name: "description",
             message: "Please describe your project (Required)",
             validate: descriptionInput => {
-                (descriptionInput) {
+               if (descriptionInput) {
                     return true;
                 } else {
                     console.log("You must enter a project description.");
                     return false;
                 }
             }
-            },
+        },
     {
-        type: 'rawlist',
-        name: 'lisence',
-        message: 'Which license does your project need?',
-        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC License', 'None']
-        validate: license => {
-            if (license) {
-                return true;
-            } else if (!license){
-                console.log('Please enter required information');
-                return false;
-            }
-        }
+        type: "input",
+        name: "lisence",
+        message: "What lisence are you using?",
     },
-    ];
+    {
+
+    type: "input",
+    name: "githubLink",
+    message: "Enter your Github profile. (Optional)",
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter your email address. (Required)",
+    validate: emailInput => {
+        if (emailInput) {
+            return true;
+        } else {
+            console.log("You must enter your email address!")
+        }
+    }
+}
+])
+};
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, error => {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log("Your README has been created! Open ReadMe.md!");
+        }
+    })
+};
+
 
 // TODO: Create a function to initialize app
 function init() {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "MIT") {
+    return `[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://spdx.org/licenses/MIT.html)`
+  } else if (license == "GNU") {
+    return `[![License](https://img.shields.io/badge/license-GPL-blue)](https://www.gnu.org/licenses/gpl-3.0-standalone.html)`
+  } else if (license == "Apache") {
+    return `[![License](https://img.shields.io/badge/license-Apache-blue)](http://www.apache.org/licenses/LICENSE-2.0)`
+  } else if (license == "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -34,8 +44,40 @@ function generateMarkdown(data) {
   - [Installation](#installation)
   - [Usage](#usage)
   - [Contributing](#contributing)
-  - [Tests](#tests)
+  - [Tests](#test)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  
+  ## Description
+  #### ${data.description}
+  
+
+  ## Installation 
+  #### ${data.installation}
+  
+  ## Usage
+  #### ${data.usage} 
+  
+  ## Contribution 
+  #### ${data.contributing}
+  
+  ## Tests
+  #### ${data.test}
+  
+  ## License 
+  #### ${renderLicenseBadge(data.license)}
+  #### ${renderLicenseSection(data.license)} 
+  #### ${renderLicenseLink(data.licenseLink)} 
+  
+  ## Questions
+  #### If you have additional questions or would like to get in touch with me, reach out using a method below
+  #### Github UserName - @${data.github}
+  #### [Github Profile](${data.githubLink})
+  #### [Email](${data.email})
+   
 `;
+
 }
 
 module.exports = generateMarkdown;
