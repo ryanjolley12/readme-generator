43eaa857afc03ec4e2072688598e417ba06d2d62
ryanjolley12 <ryanswims@gmail.com>
2021-07-20 14:08:53 -0400

    changed object array

---
+++ Develop/ReadMe.md
@@ -0,0 +1,41 @@
+
+  # Project Title 
+  ## ryan
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#test)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  
+  ## Description
+  #### fun
+  
+
+  ## Installation 
+  #### n/a
+  
+  ## Usage
+  #### n'a 
+  
+  ## Contribution 
+  #### ok
+  
+  ## Tests
+  #### ok
+  
+  ## License 
+  #### undefined
+  ####  
+  #### hi 
+  
+  ## Questions
+  #### If you have additional questions or would like to get in touch with me, reach out using a method below
+  #### Github UserName - @ryanjolley12
+  #### [Github Profile](test)
+  #### [Email](test)
+   
--- Develop/index.js
+++ Develop/index.js
@@ -1,94 +1,144 @@
-// TODO: Include packages needed for this application
+// packages needed for this application: fs and inquirer
+const fs = require("fs");
 const inquirer = require("inquirer");
-const fs = require ("fs");
-const generateMarkdown = require("./utils/generateMarkdown");
-const { type } = require("os");
+const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = () => {
+//create variable where the file will be written to and copied. Must have relative file path name
+// const { writeFile, copyFile } = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input (starter code)
+var questionsPrompt = () => {
     return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "What is the name of your project? (Required) ",
+            message: "What is the name of your project? (Required)",
             validate: nameInput => {
                 if (nameInput) {
                     return true;
-         } else  {
-             console.log("You must enter the name of your project.");
-             return false;
-         }
-            
-        }
+                } else {
+                    console.log("You must enter the name of your project.");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "description",
             message: "Please describe your project (Required)",
             validate: descriptionInput => {
-               if (descriptionInput) {
+                if (descriptionInput) {
                     return true;
                 } else {
-                    console.log("You must enter a project description.");
+                    console.log("You must enter a brief description of your project.");
                     return false;
                 }
             }
         },
-    {
-        type: "input",
-        name: "lisence",
-        message: "What lisence are you using?",
-    },
-    {
-
-    type: "input",
-    name: "githubLink",
-    message: "Enter your Github profile. (Optional)",
-},
-{
-    type: "input",
-    name: "email",
-    message: "Please enter your email address. (Required)",
-    validate: emailInput => {
-        if (emailInput) {
-            return true;
-        } else {
-            console.log("You must enter your email address!")
+        {
+            type: "confirm",
+            name: "confirmTable",
+            message: "Type Y or Yes to have your Table of Contents generated. (Required) "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What is the installation process, if any, for your project? (Optional)"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide instructions for use. (Required)",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log("You must provide basic instructions for use.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "If you would like other developers to contribute to your project, please add guidelines for how to do so. (Optional)"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "What tests, if any, have you written for your application? (Optional)"
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "If your project is licesened, please select the appropriate option (Optional)",
+            choices: ["Apache", "GNU", "MIT", "ISC", "Other"]
+        },
+        {
+            type: "input",
+            name: "licenseLink",
+            message: "If liscensed, please enter your license link here. (Optional)"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your Github username (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("You must enter your Github username");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "githubLink",
+            message: "If you'd like, enter the link to your Github profile. (Optional)"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address. (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("You must enter your email address!")
+                }
+            }
         }
-    }
-}
-])
+    ])
 };
 
+
+
+
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data) {   //starter code
     fs.writeFile(fileName, data, error => {
         if (error) {
             return console.log(error);
         } else {
-            console.log("Your README has been created! Open ReadMe.md!");
+            console.log("Your README has been generated! Check out 'ReadMe.md' to see!");
         }
     })
+
 };
 
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-    .then(questions => {
+    questionsPrompt()
+        .then(userAnswers => {
 
-        const markupHtml = writeToFile(questions);
-        fs.writeFile("./dist/README.md", markupHtml, err =>{
-            if(err){
-                console.log(err);
-            } else {
-                console.log('Success!');
-            }
+            let markdown = generateMarkdown(userAnswers);
+            console.log(markdown);
+            writeToFile("./ReadMe.md", markdown);
         })
-    })
-    
-};
+}                     
 
 // Function call to initialize app
-init();
+init();                             
